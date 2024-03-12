94c5642160e5cca977afa275c80345906eb15de3
Marcio Machado Pereira <marcio.machado.pereira@gmail.com>
2024-03-12 09:57:38 -0300

    Included unimpaired.nvim.

---
+++ lua/user/nvim_unimpaired.lua
@@ -0,0 +1,11 @@
+local M = {
+    'tummetott/unimpaired.nvim',
+  }
+
+M.config = function()
+    require('unimpaired').setup {
+        -- add any options here or leave empty
+    }
+end
+
+return M
