ef51f6be309fe37a71193912ff12a4c0cce0b32e
Marcio Machado Pereira <marcio.machado.pereira@gmail.com>
2024-03-18 16:08:12 -0300

    Customize nord colorscheme.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,12 +1,37 @@
+-- local M = {
+--   -- "shaunsingh/nord.nvim",
+--   "gbprod/nord.nvim",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+-- }
+--
+-- function M.config()
+--     require("Comment").setup({
+--         transparent = true, -- Enable this to disable setting the background color
+--     }),
+--     vim.cmd.colorscheme "nord"
+-- end
+--
+-- return M
+--
+
 local M = {
-  -- "shaunsingh/nord.nvim",
   "gbprod/nord.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  lazy = false, -- This ensures the plugin is loaded during startup, which is what you want for a colorscheme
+  priority = 1000, -- Ensures this plugin is loaded before others
 }
 
 function M.config()
-  vim.cmd.colorscheme "nord"
+    -- Ensure nord is configured only if it's already loaded
+    local status_ok, nord_color = pcall(require, "nord")
+    if status_ok then
+        nord_color.setup({
+            transparent = true, -- Disable setting the background color
+        })
+    end
+
+    -- Set the colorscheme
+    vim.cmd("colorscheme nord")
 end
 
 return M
